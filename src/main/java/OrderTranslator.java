f4daddf979ab4a25afd2c45cbb27b0ee317d5150
Robin Welsch <rwelsch@orange.fr>
2022-01-22 18:12:51 +0100

    First iteration: implementation

--- src/main/java/OrderTranslator.java
+++ src/main/java/OrderTranslator.java
@@ -3,6 +3,22 @@ public class OrderTranslator {
     private DrinkMaker drinkMaker;
 
     public boolean translateOrder(BeverageOrder beverageOrder) {
-        return false;
+        String command = switch(beverageOrder.getBeverageType()) {
+            case COFFEE -> "C";
+            case TEA -> "T";
+            case CHOCOLATE -> "H";
+            default -> null;
+        };
+        if(command == null) {
+            return false;
+        }
+        if(beverageOrder.getSugarAmount() > 0) {
+            command = command.concat(":" + beverageOrder.getSugarAmount());
+            command = command.concat(":" + (beverageOrder.isHasStick() ? 1 : 0));
+        } else {
+            command = command.concat("::");
+        }
+        drinkMaker.makeDrinks(command);
+        return true;
     }
 }
